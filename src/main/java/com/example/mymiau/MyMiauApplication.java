5fd697c981162315ca7fd9dee313e5d4b52cea96
maffzz <maria.lazon@utec.ued.pe>
2025-04-30 21:04:01 -0500

    les't go

---
+++ src/main/java/com/example/mymiau/MyMiauApplication.java
@@ -0,0 +1,9 @@
+package com.example.mymiau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMiauApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyMiauApplication.class, args);}}
---
+++ src/test/java/com/example/mymiau/MyMiauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymiau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMiauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
